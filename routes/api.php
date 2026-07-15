60331da98d8b81dcb6fe52055809a0560a4baf82
iMumd <hi@mumd.dev>
2026-07-15 12:34:21 +0300

    Wire auth and invite-link routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\InviteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/invite/{token}/accept', [InviteController::class, 'accept']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/me/change-password', [AuthController::class, 'changePassword']);
+
+    Route::middleware('force-password-change')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::get('/me/abilities', [AuthController::class, 'abilities']);
+        Route::post('/users/{user}/invite', [InviteController::class, 'invite']);
+    });
+});
